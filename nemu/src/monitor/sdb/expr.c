2a17a6389a56ed4989aab5941542c8dbbce9cef9
hartvon <fhtzvl@gmail.com>
2024-07-11 17:38:11 +0800

    finish p1.2

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -99,13 +99,17 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-        tokens[nr_token].type = rules[i].token_type;
-        strncpy(tokens[nr_token].str, substr_start, substr_len);
-        ++nr_token;
+        if (rules[i].token_type == TK_NOTYPE) {
+          break;
+        }
 
-        // switch (rules[i].token_type) {
-        //   default: TODO();
-        // }
+        switch (rules[i].token_type) {
+          case TK_DEC:
+          case TK_EQ:
+            tokens[nr_token].type = rules[i].token_type;
+            strncpy(tokens[nr_token].str, substr_start, substr_len);
+        }
+        ++nr_token;
 
         break;
       }
@@ -139,8 +143,52 @@ bool check_parentheses(uint32_t p, uint32_t q) {
   return count == 0;
 }
 
-uint32_t find_major(char* e) {
-  TODO();
+int get_precedence(int tk_type) {
+  switch (tk_type) {
+    case '*': case '/': return 2;
+    case '+': case '-': return 1;
+    default: panic("Unsupported token");
+  }
+  return 0;  // Should never reach here
+}
+
+int find_major(int p, int q) {
+  int ret = 0, paren_cnt = 0, opt_type = 0;
+
+  for (int i = p; i <= q; ++i) {
+    int tk_type = tokens[i].type;
+
+    if (tk_type == TK_DEC) {
+      continue;
+    }
+
+    if (tk_type == '(') {
+      ++paren_cnt;
+    }
+    else if (tk_type == ')') {
+      if (paren_cnt == 0) {
+        return -1;
+      }
+      --paren_cnt;
+    }
+    else {
+      if (paren_cnt > 0) {
+        continue;
+      }
+
+      int tmp_type = get_precedence(tk_type);
+
+      if (tmp_type >= opt_type) {
+        opt_type = tmp_type;
+        ret = i;
+      }
+    }
+  }
+
+  if (paren_cnt != 0) {
+    return -1;
+  }
+  return ret;
 }
 
 word_t eval(int p, int q, bool* success) {
@@ -159,11 +207,31 @@ word_t eval(int p, int q, bool* success) {
     return eval(p + 1, q - 1, success);
   }
   else {
-    TODO();
+    int major_pos = find_major(p, q);
+    if (major_pos == -1) {
+      *success = false;
+      return 0;
+    }
+
+    word_t val1 = eval(p, major_pos - 1, success);
+    if (*success == false) return 0;
+
+    word_t val2 = eval(major_pos + 1, q, success);
+    if (*success == false) return 0;
+
+    switch (tokens[major_pos].type) {
+      case '+': return val1 + val2;
+      case '-': return val1 - val2;
+      case '*': return val1 * val2;
+      case '/': if (val2 == 0) {
+        *success = false;
+        return 0;
+      }
+      return val1 / val2;
+      default: panic("Unsupported opt");
+    }
   }
 }
-
-// tokens | type:str
 
 word_t expr(char *e, bool *success) {
   if (!make_token(e)) {
@@ -172,6 +240,4 @@ word_t expr(char *e, bool *success) {
   }
 
   return eval(0, nr_token - 1, success);
-
-  return 0;
 }
